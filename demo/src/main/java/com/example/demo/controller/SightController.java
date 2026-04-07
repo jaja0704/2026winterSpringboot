92de44d8fc1cbc17597e5c0b0e4248e62c9f10fd
jaja0704 <arthur870704@gmail.com>
2026-04-07 15:24:20 +0800

    fix jar and docker file

--- demo/src/main/java/com/example/demo/controller/SightController.java
+++ demo/src/main/java/com/example/demo/controller/SightController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
 
-import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
@@ -15,10 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.KeelungSightsCrawler;
 import com.example.demo.Sight;
